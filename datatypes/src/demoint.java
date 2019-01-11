8b51d89b5d05d385c21413c6388473e95ef4bbaf
Jatin Thakran <jatin18csu095@ncuindia.edu>
2019-01-11 10:58:17 +0530

    datatypes

---
+++ datatypes/src/demoint.java
@@ -0,0 +1,17 @@
+class demoint
+{
+public static void main(String args[])
+{
+int a=1,b=2,add,sum,mul,div,mod;
+add=a+b;
+sum=b-a;
+mul=a*b;
+div=a/b;
+mod=b%a;
+System.out.println(add);
+System.out.println(sum);
+System.out.println(mod);
+System.out.println(mul);
+System.out.println(div);
+}
+}
---
+++ datatypes/src/util.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class util
+{
+public static void main(String args[])
+{
+Scanner a = new Scanner(System.in);
+int b,c,sum;
+b=a.nextInt();
+c=a.nextInt();
+sum=b+c;
+System.out.println(sum);
+}
+}
